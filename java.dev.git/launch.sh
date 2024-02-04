a2cc3c508a1a0411aa056761331418a15a642ee0
FlavienNoualhaguet <noualhaguet.flavien@gmail.com>
2024-02-04 10:52:26 +0100

    lauch.sh

--- java.dev.git/launch.sh
+++ java.dev.git/launch.sh
@@ -21,13 +21,17 @@ if [[ ! -z "$CONTAINER_NAME" ]]; then
         # Entrer dans le conteneur avec une session bash
         docker exec -it "$CONTAINER_NAME" /bin/bash
     fi
+    RC=$?
+    exit $RC
 else
     # Le conteneur n'existe pas
     echo "Le conteneur n'existe pas. Création et démarrage..."
     docker run -it --name "${USER}.java.dev.git.V1.0" -e DISPLAY=$DISPLAY -e WAYLAND_DISPLAY=$WAYLAND_DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --entrypoint /bin/bash $IMAGE_NAME
-    if [[ $$ -ne 0 ]]; then
+    RC=$?
+    if [[ $RC -ne 0 ]]; then
         echo "Si il y a un probleme de display: xhost +"
         echo "relancer $(basename $0)"
+        exit $RC
     fi
 fi
 
